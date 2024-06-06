682eeeccf88b12fc24679757bfc3ca876147eb02
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-06 13:13:46 +0530

    file updated

--- BasicPrograms/factorial.java
+++ BasicPrograms/factorial.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 public class factorial {
 
-public int factorial(int num){
+public int fact (int num){
 if(num == 1 || num == 0){
     return 1;
 }
 else{
-    return num*factorial(num-1);
+    return num*fact(num-1);
 }
 }
     public static void main (String [] agrs){
@@ -16,7 +16,7 @@ else{
         System.out.println("Enter a number whose factorial you want to calculate\n");
         int number = sc.nextInt();
         factorial obj = new factorial();
-        System.out.println(obj.factorial(number));
+        System.out.println(obj.fact(number));
         sc.close();
     }
 }
