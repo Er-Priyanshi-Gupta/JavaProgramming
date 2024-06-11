c161232343e713dd83a08f9fc464e1ac8e96c7b2
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-11 18:59:38 +0530

    file updated

--- NumberBasedPrograms/happy.java
+++ NumberBasedPrograms/happy.java
@@ -1,5 +1,37 @@
 package JavaProgramming.NumberBasedPrograms;
-
+import java.util.Scanner;
 public class happy {
-    
+    public static int sod(int num){
+        int d = 0 ;
+     int square = 0;
+    while (num>0){
+        d = num%10;
+        square = square + (int)Math.pow(d,2);
+        num/=10;
+        }  
+    return square;
+    }
+    public static boolean isHappy(int num){
+        int happy = sod(num);
+    while(true){
+        if (happy == 1){
+            return true;
+               }      
+        else if (happy>9) 
+        happy = sod(happy);
+        else
+         return false;
+    }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check whether it is Happy or not.");
+        int number = sc.nextInt();
+        if (isHappy(number)) {
+            System.out.println("Number " + number + " is a Happy number");
+        } else {
+            System.out.println("Number " + number + " is not a Happy number");
+        }
+        sc.close();
+    }
 }
