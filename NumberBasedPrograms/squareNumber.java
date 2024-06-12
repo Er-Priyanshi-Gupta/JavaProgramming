7158aa244e0929272649393d686d97489e7bd3d5
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-12 15:59:22 +0530

    file updated

--- NumberBasedPrograms/squareNumber.java
+++ NumberBasedPrograms/squareNumber.java
@@ -1,18 +1,16 @@
 package JavaProgramming.NumberBasedPrograms;
-
 import java.util.Scanner;
-
 public class squareNumber {
-   
-    
-
-
-
-
-
-
-
-
+public static int square(int num){
+    return (int)Math.pow((int)Math.sqrt(num),2);
+}
+public static boolean isSquare(int num){
+if (num == square(num)){
+    return true;
+}
+else 
+return false;
+}
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a number to check whether it is Square or not.");
