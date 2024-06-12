b3c2e7de2b847bea8671943179808d4811765991
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-12 15:34:00 +0530

    file updated

--- NumberBasedPrograms/niven.java
+++ NumberBasedPrograms/niven.java
@@ -1,5 +1,32 @@
 package JavaProgramming.NumberBasedPrograms;
-
+import java.util.Scanner;
 public class niven {
-    
+    public static int sod(int num){
+        int d = 0;
+        int sum = 0;
+        while(num>0){
+            d = num%10;
+            sum+= d;
+            num/= 10;
+        }
+        return sum;
+    }
+    public static boolean isNiven(int num){
+        if (num%sod(num)==0){
+            return true;
+        }
+        else
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check whether it is Niven or not.");
+        int number = sc.nextInt();
+        if (isNiven(number)) {
+            System.out.println("Number " + number + " is Niven");
+        } else {
+            System.out.println("Number " + number + " is not Niven");
+        }
+        sc.close();
+    }
 }
--- NumberBasedPrograms/smith.java
+++ NumberBasedPrograms/smith.java
@@ -1,5 +1,25 @@
 package JavaProgramming.NumberBasedPrograms;
 
+import java.util.Scanner;
+
 public class smith {
     
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number to check whether it is Kaprekar or not.");
+    int number = sc.nextInt();
+    if (isKaprekar(number)) {
+        System.out.println("Number " + number + " is Kaprekar");
+    } else {
+        System.out.println("Number " + number + " is not Kaprekar");
+    }
+    sc.close();
+}
 }
