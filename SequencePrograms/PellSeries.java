97257b08e50a6f938f9e65390cedb6947aefbfc7
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-21 14:15:41 +0530

    file updated

--- SequencePrograms/PellSeries.java
+++ SequencePrograms/PellSeries.java
@@ -1,5 +1,25 @@
 package JavaProgramming.SequencePrograms;
 import java.util.Scanner;
 public class PellSeries {
+    public static int pell(int num){
+        if(num == 1){
+            return 0;
+        }
+        else if (num == 2){
+            return 1;
+        }
+        else{
+            return 2*pell(num-1)+ pell(num-2);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a length of the Pell sequence \n");
+        int number = sc.nextInt();
     
+        for(int i = 1; i<=number ;i++){
+            System.out.print(pell(i)+" ");
+        }
+        sc.close();
+}
 }
