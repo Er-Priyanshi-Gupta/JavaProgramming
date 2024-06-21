263fc55010f62e25b5b6543c1994d61db0df8b61
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-21 14:15:35 +0530

    file updated

--- SequencePrograms/PerrinSequence.java
+++ SequencePrograms/PerrinSequence.java
@@ -1,5 +1,28 @@
 package JavaProgramming.SequencePrograms;
 import java.util.Scanner;
 public class PerrinSequence {
+    public static int perrin(int num){
+        if(num == 1){
+            return 3;
+        }
+        else if (num == 2){
+            return 0;
+        }
+        else if (num == 3){
+            return 2;
+        }
+        else{
+            return perrin(num-2)+ perrin(num-3);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a length of the Perrin sequence \n");
+        int number = sc.nextInt();
     
+        for(int i = 1; i<=number ;i++){
+            System.out.print(perrin(i)+" ");
+        }
+        sc.close();
+}
 }
