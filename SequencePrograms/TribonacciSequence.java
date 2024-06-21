82ff5524970d7f58c4355f8f12c87298779ec80c
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-21 14:15:24 +0530

    file updated

--- SequencePrograms/TribonacciSequence.java
+++ SequencePrograms/TribonacciSequence.java
@@ -1,5 +1,28 @@
 package JavaProgramming.SequencePrograms;
 import java.util.Scanner;
 public class TribonacciSequence {
+    public static int tribonacci(int num){
+        if(num == 1){
+            return 0;
+        }
+        else if (num == 2){
+            return 0;
+        }
+        else if (num == 3){
+            return 1;
+        }
+        else{
+            return tribonacci(num-1)+ tribonacci(num-2)+ tribonacci(num-3);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a length of the Tribonacci sequence \n");
+        int number = sc.nextInt();
     
+        for(int i = 1; i<=number ;i++){
+            System.out.print(tribonacci(i)+" ");
+        }
+        sc.close();
+}
 }
