f45f96d77739bb9a78106ba544ad946f881ba669
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-22 08:36:57 +0530

    file updated

--- BasicPrograms/voter.java
+++ BasicPrograms/voter.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class voter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name\n");
+        System.out.println("Enter your name");
         String name = sc.next();
-        System.out.println("Enter your age\n");
+        System.out.println("Enter your age");
         int age = sc.nextInt();
         if(age>18){
-            System.out.println(name+" is an adult and is eligible to vote\n");
+            System.out.println(name+" is an adult and is eligible to vote");
         }
         else{
-            System.out.println(name+" is not an adult and is not eligible to vote\n");
+            System.out.println(name+" is not an adult and is not eligible to vote");
         }
         sc.close();
     }
