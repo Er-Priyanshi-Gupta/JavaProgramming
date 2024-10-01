bbf3b896292793c548dc1224c91bce4acda8a5ae
Priyanshi Gupta <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-10-01 12:55:31 +0530

    .

--- PatternPrograms/InvertedTriangle.java
+++ PatternPrograms/InvertedTriangle.java
@@ -1,5 +1,18 @@
 package JavaProgramming.PatternPrograms;
 
+import java.util.Scanner;
+
 public class InvertedTriangle {
-    
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number of rows");
+         int n = sc.nextInt();
+         for(int i = n; i<=1;i++){
+            for(int j = 1; j<=i; j++){
+                System.err.print("*");
+            }
+            System.err.println("");
+         }
+         sc.close();
+    }
 }
--- PatternPrograms/Triangle.java
+++ PatternPrograms/Triangle.java
@@ -1,5 +1,18 @@
 package JavaProgramming.PatternPrograms;
+
 import java.util.Scanner;
+
 public class Triangle {
-    
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number of rows");
+         int n = sc.nextInt();
+         for(int i = 1; i<=n;i++){
+            for(int j = 1; j<=i; j++){
+                System.err.print("*");
+            }
+            System.err.println("");
+         }
+         sc.close();
+    }
 }
